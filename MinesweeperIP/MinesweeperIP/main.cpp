b0517c9e4fd4054ccd4802d9a11d6d3eea6392d1
Bogdan Raducan <bogdan.raducan@emag.ro>
2016-12-14 22:35:08 +0200

    Initial commit

---
+++ MinesweeperIP/MinesweeperIP/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  MinesweeperIP
+//
+//  Created by Bogdan Raducan on 14/12/2016.
+//  Copyright © 2016 Bogdan Raducan. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    cout << "Hello, World!\n";
+    return 0;
+}
