175598bb573f2a6a9e502ae19ab7a0f9f8ae0b30
Bogdan Raducan <bogdan.raducan@emag.ro>
2016-12-27 19:35:02 +0200

    Add class for board tile

---
+++ MinesweeperIP/MinesweeperIP/Tile.cpp
@@ -0,0 +1,9 @@
+//
+//  Tile.cpp
+//  MinesweeperIP
+//
+//  Created by Bogdan Raducan on 14/12/2016.
+//  Copyright © 2016 Bogdan Raducan. All rights reserved.
+//
+
+#include "Tile.hpp"
---
+++ MinesweeperIP/MinesweeperIP/Tile.hpp
@@ -0,0 +1,14 @@
+//
+//  Tile.hpp
+//  MinesweeperIP
+//
+//  Created by Bogdan Raducan on 14/12/2016.
+//  Copyright © 2016 Bogdan Raducan. All rights reserved.
+//
+
+#ifndef Tile_hpp
+#define Tile_hpp
+
+#include <stdio.h>
+
+#endif /* Tile_hpp */
