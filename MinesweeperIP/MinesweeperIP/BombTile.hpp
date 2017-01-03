ca84be0ced20862a478b4e6d1fb7db2e9848c2d4
Bogdan Raducan <bogdan.raducan@emag.ro>
2017-01-03 15:09:33 +0200

    Add subclasses for tiles that are bombs and those that aren’t

---
+++ MinesweeperIP/MinesweeperIP/BombTile.hpp
@@ -0,0 +1,17 @@
+//
+//  BombTile.hpp
+//  MinesweeperIP
+//
+//  Created by Bogdan on 03/01/17.
+//  Copyright © 2017 Bogdan Raducan. All rights reserved.
+//
+
+#ifndef BombTile_hpp
+#define BombTile_hpp
+
+#include "Tile.hpp"
+
+class BombTile : public Tile {
+};
+
+#endif /* BombTile_hpp */
---
+++ MinesweeperIP/MinesweeperIP/ValueTile.hpp
@@ -0,0 +1,21 @@
+//
+//  ValueTile.hpp
+//  MinesweeperIP
+//
+//  Created by Bogdan on 03/01/17.
+//  Copyright © 2017 Bogdan Raducan. All rights reserved.
+//
+
+#ifndef ValueTile_hpp
+#define ValueTile_hpp
+
+#include "Tile.hpp"
+
+#include <stdio.h>
+
+class ValueTile : Tile {
+public:
+    int value;
+};
+
+#endif /* ValueTile_hpp */
