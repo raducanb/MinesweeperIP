82a5d6805f36040cd230b7699a85d9125e801c92
Bogdan Raducan <bogdan.raducan@emag.ro>
2017-01-03 17:27:10 +0200

    Add initial class for the board

---
+++ MinesweeperIP/MinesweeperIP/Board/Board.cpp
@@ -0,0 +1,9 @@
+//
+//  Board.cpp
+//  MinesweeperIP
+//
+//  Created by Bogdan on 03/01/17.
+//  Copyright © 2017 Bogdan Raducan. All rights reserved.
+//
+
+#include "Board.hpp"
---
+++ MinesweeperIP/MinesweeperIP/Board/Board.hpp
@@ -0,0 +1,14 @@
+//
+//  Board.hpp
+//  MinesweeperIP
+//
+//  Created by Bogdan on 03/01/17.
+//  Copyright © 2017 Bogdan Raducan. All rights reserved.
+//
+
+#ifndef Board_hpp
+#define Board_hpp
+
+#include "Position.cpp"
+
+#endif /* Board_hpp */
